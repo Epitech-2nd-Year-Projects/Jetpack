34294bbd604fb3c6f7429a51801ed5a46e9d6129
Yanis <yanis.kernoua@epitech.eu>
2025-04-19 13:34:53 +0200

    fix: update player starting position in checkGameStart method

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -333,13 +333,7 @@ void Jetpack::Server::GameServer::checkGameStart() {
 
     for (auto &[_, player] : m_players) {
       player.setState(Shared::Protocol::PlayerState::READY);
-
-      constexpr float startX = 1.0f;
-      const float startY = m_map.height / 2.0f;
-
-      player.setPosition(startX, startY);
-      player.setVelocityY(0.0f);
-      player.setScore(0);
+      player.setPosition(1.0f, 1.0f);
     }
 
     m_broadcaster.broadcastGameStart();
